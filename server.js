a6fae8d697ed4bd8d7268625ba12f054dd6ef9c1
Kuriti Sivasai <kuritisivasi@gmail.com>
2026-08-18 21:51:39 +0530

    Fix Vercel static file routing

--- server.js
+++ server.js
@@ -2,21 +2,18 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
-// Database Connection
 require("./config/db");
 
-// Routes
 const authRoutes = require("./routes/authRoutes");
 const passwordRoutes = require("./routes/passwordRoutes");
 
 const app = express();
 
-// Middleware
 app.use(cors());
 app.use(express.json());
+
 app.use(express.static("public"));
 
-// Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/password", passwordRoutes);
 
@@ -24,5 +21,4 @@ app.get("/", (req, res) => {
     res.sendFile(__dirname + "/public/index.html");
 });
 
-// Export for Vercel
 module.exports = app;
